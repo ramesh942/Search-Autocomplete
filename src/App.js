93683bfc5979bb1aae9c468387b4350696502b29
ramesh942 <rameshpawar1995@gmail.com>
2020-03-18 11:37:43 +0530

    public and src

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import AutoCompletedText from './Contaners/AutoCompletedText'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AutoCompletedText />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/Contaners/AutoCompletedText.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import Country from "../Contaners/Countries";
+export default class AutoCompletedText extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      suggestions: [],
+      text: ""
+    };
+  }
+  onTextChange = e => {
+    const value = e.target.value;
+    let suggestions = [];
+    if (value.length > 0) {
+      const regex = new RegExp(`^${value}`, "i");
+      suggestions = Country.sort().filter(v => regex.test(v));
+    }
+
+    this.setState(() => ({
+      suggestions,
+      text: value
+    }));
+  };
+
+  selectedText(value) {
+    this.setState(() => ({
+      text: value,
+      suggestions: []
+    }));
+  }
+  renderSuggestions = () => {
+    let { suggestions } = this.state;
+    if (suggestions.length === 0) {
+      return null;
+    }
+    return (
+      <ul>
+        {suggestions.map((item, index) => (
+          <li key={index} onClick={() => this.selectedText(item)}>
+            {item}
+          </li>
+        ))}
+        
+      </ul>
+    );
+  };
+
+  render() {
+    const { text, suggestions } = this.state;
+    return (
+      <div id="notebooks">
+        <h2>Auto Completed</h2>
+        <input
+          id="query"
+          type="text"
+          onChange={this.onTextChange}
+          value={text}
+        />
+        {this.renderSuggestions()}
+        <span>Suggestions:{suggestions.length}</span>
+      </div>
+    );
+  }
+}
